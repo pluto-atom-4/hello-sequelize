8b78bd774f6c1ff0877a18ecf17e656d2701fabc
pluto-atom-4 <genko2@hotmail.com>
2019-01-06 06:49:58 -0800

    adds
    resource file to point the config.js in stead of config.config

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'expressuser',
+    password: 'ThePassword',
+    database: 'hello_sequelize',
+    host: 'nuc-coreos',
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+// dialectOptions: {
+//   ssl: {
+//     ca: fs.readFileSync(__dirname + '/mysql-ca-master.crt')
+//   }
+// }
+  }
+};
